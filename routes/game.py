6ebdd37fe4e107ddf4529bbea9f631c12410e7fb
MarilynPam <166864020+MarilynPam@users.noreply.github.com>
2025-04-16 16:14:33 -0400

    Update game.py
    Added /random question route

--- routes/game.py
+++ routes/game.py
@@ -1,7 +1,30 @@
 from fastapi import APIRouter
+from db import get_connection
 
 router = APIRouter()
 
-@router.get("/test")
-def test_game_route():
-    return {"message": "Game routes are working!"}
+@router.get("/random")
+def get_random_question():
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    # Get a random question
+    cursor.execute("SELECT * FROM Questions ORDER BY RANDOM() LIMIT 1")
+    question = cursor.fetchone()
+
+
+    if question is None: 
+        return {"Error": "No questions found"}
+
+    question_id, question_type, question_text = question
+
+    # Get all associated answers
+    cursor.execute("SELECT AnswerID, AnswerString FROM Answers WHERE QuestionID=?", (question_id,))
+    answers = cursor.fetchall()
+
+    return {
+        "question_id": question_id,
+        "question_type": question_type,
+        "question_text": question_text,
+        "answers": [{"id": aid, "text":atext} for (aid, atext) in answers]
+    }
