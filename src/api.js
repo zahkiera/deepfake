f91f82bdbd5f3fa243462f1eaa0394e3639fc3a3
MarilynPam <166864020+MarilynPam@users.noreply.github.com>
2025-04-16 15:09:57 -0400

    Create api.js
    Set up API utility file

---
+++ src/api.js
@@ -0,0 +1,27 @@
+const API_BASE_URL = "http://127.0.0.1:8000/api";  // Change when/if we deploy
+
+// Register a new user
+export async function registerUser(username, password) {
+  const res = await fetch(`${API_BASE_URL}/user/register`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ username, password }),
+  });
+  return await res.json();
+}
+
+// Log in user
+export async function loginUser(username, password) {
+  const res = await fetch(`${API_BASE_URL}/user/login`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ username, password }),
+  });
+  return await res.json();
+}
+
+// Get question with answers
+export async function getQuestion(questionId) {
+  const res = await fetch(`${API_BASE_URL}/game/${questionId}`);
+  return await res.json();
+}
